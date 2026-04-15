ab5fb06691be4f45f8be79125e8bf8af586f044e
Robert Orozco <mauricio_101295@outlook.com>
2026-04-15 10:46:38 -0500

    Dia 3 Terraform

---
+++ day3/main.tf
@@ -0,0 +1,44 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+  shared_credentials_files = ["C:/Users/Usuario/.aws/credentials"]
+  profile = "terraform"
+}
+
+resource "aws_instance" "webserver" {
+  ami = "ami-07062e2a343acc423" #Ubuntu Server 24.04 LTS
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  tags = {
+    Name = "terraform-test"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y apache2
+              echo "Mi primer deploy en terraform!!" > /var/www/html/index.html
+              systemctl enable apache2
+              systemctl start apache2
+              EOF
+
+  user_data_replace_on_change = true
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = var.server_port
+    to_port = var.server_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
